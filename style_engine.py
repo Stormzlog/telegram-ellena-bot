c9f77086cb2c753d17cbc8d4009e227194f69420
Gabriel Anum <115888083+Stormzlog@users.noreply.github.com>
2026-02-03 13:01:28 +0100

    Implement apply_style function in style_engine.py
    Add function to apply random styling to text based on length

--- style_engine.py
+++ style_engine.py
@@ -1,1 +1,7 @@
+import random
 
+def apply_style(text):
+    if len(text) > 60 and random.random() > 0.6:
+        mid = len(text)//2
+        return text[:mid] + "\n\n" + text[mid:]
+    return text
